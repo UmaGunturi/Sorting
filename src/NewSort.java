c54bce8a42bd171f972048941abf982cdf451df6
Sushmitha2000 <Hp@Sushmitha_2000>
2019-02-01 10:03:35 +0530

    merge branch commit for sorting

--- src/NewSort.java
+++ src/NewSort.java
@@ -74,7 +74,7 @@ public class NewSort {
 		    // Driver method 
 		    public static void main(String args[]) 
 		    { 
-		        int arr[] = {12, 11, 13, 5, 6, 7}; 
+		        int arr[] = {12, 11, 13, 5, 6, 7,8}; 
 		  
 		        System.out.println("Given Array"); 
 		        printArray(arr); 
